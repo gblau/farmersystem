d6743caacd80c7af82be25595ae5bc997a8f19f3
gblau <gblau@msn.com>
2017-08-22 14:46:16 +0800

    异常处理代码修改

--- src/main/Java/com/gb/common/GlobalControllerExceptionHandler.java
+++ src/main/Java/com/gb/common/GlobalControllerExceptionHandler.java
@@ -3,14 +3,21 @@ package com.gb.common;
 import com.gb.common.util.Log;
 import com.gb.model.vo.ResponseModel;
 import org.slf4j.Logger;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.ResponseEntity.*;
 
 /**
- * 全局Controller异常捕捉类，一般用于对@Valid、@RequestParam、@RequestBody等注解参数不符合要求时抛出的异常进行处理。
+ * 全局Controller异常捕捉类
+ * 可以用于对{@link Valid}、{@link RequestParam}、{@link RequestBody} 等注解参数不符合要求时抛出的异常进行处理。
  * @author gblau
  * @date 2017-04-22
  */
@@ -27,7 +34,7 @@ public class GlobalControllerExceptionHandler {
     public ResponseEntity<ResponseModel> handleUncaughtedException(Exception e) {
         Log.error(logger, "全局异常处理捕捉到一个错误。", e);
         //ResponseEntity和@RequestBody的不同在于，他可以定义自己的返回状态码
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ResponseModel.internerServerError().build());
+        return status(INTERNAL_SERVER_ERROR).body(ResponseModel.internerServerError().build());
     }
 
     /**
@@ -38,6 +45,6 @@ public class GlobalControllerExceptionHandler {
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<ResponseModel> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
         Log.info(logger, e.getLocalizedMessage());
-        return ResponseEntity.badRequest().body(ResponseModel.badRequest().message(""));
+        return badRequest().body(ResponseModel.badRequest().message(""));
     }
 }
