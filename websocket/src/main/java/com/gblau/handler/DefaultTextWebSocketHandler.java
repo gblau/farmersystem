3ce7c1c428a17ff3d4d53524ee69df8f3e0f50f4
gblau <gblau@msn.com>
2017-09-01 10:35:52 +0800

    删除自实现handler

--- websocket/src/main/java/com/gblau/handler/DefaultTextWebSocketHandler.java
+++ websocket/src/main/java/com/gblau/handler/DefaultTextWebSocketHandler.java
@@ -6,6 +6,9 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * @author gblau
  * @date 2017-08-10
@@ -13,10 +16,50 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Controller
 @RequestMapping("/websocket")
 public class DefaultTextWebSocketHandler extends TextWebSocketHandler {
+    private static final ArrayList<WebSocketSession> users = new ArrayList<>();
+
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         super.handleTextMessage(session, message);
         TextMessage returnMessage = new TextMessage(message.getPayload() + " recieved at server");
         session.sendMessage(returnMessage);
+    }
+
+    /**
+     * 给所有在线用户发送消息
+     *
+     * @param message
+     */
+    public void sendMessageToUsers(TextMessage message) {
+        for (WebSocketSession user : users) {
+            try {
+                if (user.isOpen()) {
+                    user.sendMessage(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 给某个用户发送消息
+     *
+     * @param userName
+     * @param message
+     */
+    public void sendMessageToUser(String userName, TextMessage message) {
+        for (WebSocketSession user : users) {
+            if (user.getAttributes().get("user").equals(userName)) {
+                try {
+                    if (user.isOpen()) {
+                        user.sendMessage(message);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
     }
 }
