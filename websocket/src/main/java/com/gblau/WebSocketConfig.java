bf92a1f55f4cd6dd482f701964e1cafe71eb96e3
gblau <gblau@msn.com>
2017-09-03 10:43:49 +0800

    继承WebSocketConfigurationSupport改为实现WebSocketConfigurer

--- websocket/src/main/java/com/gblau/WebSocketConfig.java
+++ websocket/src/main/java/com/gblau/WebSocketConfig.java
@@ -3,15 +3,19 @@ package com.gblau;
 import com.gblau.handler.DefaultTextWebSocketHandler;
 import com.gblau.interceptor.WebSocketInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurationSupport;
+import org.springframework.web.socket.config.annotation.DelegatingWebSocketConfiguration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 /**
- * WebMvcConfigurerAdapter
+ * 开启WebSocket配置后，所有实现{@link WebSocketConfigurer}的配置都会加入到一个configurers列表中统一维护，实现多配制共存
+ * @see DelegatingWebSocketConfiguration
+ * @see EnableWebSocket 使用Import标签将{@link DelegatingWebSocketConfiguration}引入配置。
  * @author gblau
  * @date 2017-05-20
  */
-public class WebSocketConfig extends WebSocketConfigurationSupport {
+public class WebSocketConfig implements WebSocketConfigurer {
     @Autowired
     private DefaultTextWebSocketHandler socketHandler;
 
