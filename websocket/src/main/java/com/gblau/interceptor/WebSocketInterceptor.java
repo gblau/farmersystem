cffa981f5e07bc9fc114fa4cc26c050f0db819fd
gblau <gblau@msn.com>
2017-09-05 13:35:30 +0800

    通过继承HttpSessionHandshakeInterceptor实现握手

--- websocket/src/main/java/com/gblau/interceptor/WebSocketInterceptor.java
+++ websocket/src/main/java/com/gblau/interceptor/WebSocketInterceptor.java
@@ -4,7 +4,7 @@ import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
@@ -13,28 +13,19 @@ import java.util.Map;
  * @author gblau
  * @date 2017-05-20
  */
-public class WebSocketInterceptor implements HandshakeInterceptor {
-
-    @Override
-    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
-                               WebSocketHandler handler, Exception exception) {
-    }
-
+public class WebSocketInterceptor extends HttpSessionHandshakeInterceptor {
     /**
      *  将HttpSession中对象放入WebSocketSession中
      */
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
-                                   WebSocketHandler handler, Map<String, Object> map) throws Exception {
-        if(request == null)
-            return true;
-
+                                   WebSocketHandler handler, Map<String, Object> attributes) throws Exception {
         ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
         HttpSession session = servletRequest.getServletRequest().getSession();
         if(session!=null) {
             //区分socket连接以定向发送消息
-            map.put("user", session.getAttribute("user"));
+            attributes.put("user", session.getAttribute("user"));
         }
-        return true;
+        return super.beforeHandshake(request, response, handler, attributes);
     }
 }
